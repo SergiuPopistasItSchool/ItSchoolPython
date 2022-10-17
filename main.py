b2b7a93a1d9caa92faa0abee4d1974beeb5de11c
SergiuPopistasItSchool <sergiu.popistas@itschool.ro>
2022-10-17 21:02:44 +0300

    BinaryTree si parcuregrea pe adancime

--- main.py
+++ main.py
@@ -1,82 +1,26 @@
-# This is a sample Python script.
+class Persons:
+    numer_of_persons = 0
+    def __init__(self, name):
+        self.name = name
+        self.addPersonNumer()
+    @classmethod
+    def addPersonNumer(cls):
+        Persons.numer_of_persons += 1
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-def Shout(param):
-    return  param + "!"
-message = "Hello"
-for caracter in message:
-    if caracter == "H":
-        print("STOP")
-        break
-    print(caracter)
+p = Persons("Sergiu")
+p1 = Persons("Stefan")
+print(p.numer_of_persons)
 
-print(bool(-100))
-print("This is a new test")
+a = [1, 2, 3, 4]
+b = a[::-1]
+c = b[::-2]
 
-"""
-@param_in THis
-is a
-multi c
-omment
-"""
-
-print((2**3)**2)
-
-def update():
-    table = ['a', 'd']
-print("table")
-
-my_table = ['h', 'e', 'l', 'l', 'o']
-my_var = ""
-
-for l in my_table[1:-1]:
-    my_var += l
-
-print(my_var)
-print(my_table)
-
-my_data = [0, 1, 2, 3, 4, 5, 6, 7]
-my_data_var = 0
-for i in my_data:
-    if not i % 2:
-        print(i)
-        my_data_var += i
-
-print(my_data_var)
-
-
-def divideByTwo(param):
-    #print(bool(bool)) #bool-uri by default sunt egale cu True
-    if param == 0:
-        raise ZeroDivisionError
-    elif param < 0:
-        raise Exception
-    else:
-        return param/2
-
-data = 0
-
-#divideByTwo(data)
-
-def calc(x):
-    if x < 0:
-        raise Exception
-    elif x is not type(int):
-        raise TypeError
-
-var = 'c'
-try:
-    calc(var)
-    print("Printed")
-except TypeError:
-    print("Var is not a int")
-except:
-    print("Not calculated")
-
-else:
-    print("Was caluclated")
-
+x = 1
+for i in range(5):
+    x = 0
+    #x = x + 1
+    x += 1
+print(x)
 
 
 
